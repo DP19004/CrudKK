1b9c7ed4e9bfa333ed912cd437f21be02fc71337
Santiago Tadeo Del Pinal <112456282+Elkingplayer@users.noreply.github.com>
2022-09-15 16:45:17 -0600

    Initial commit

---
+++ database/migrations/2022_09_15_183057_create_articulos_table.php
@@ -0,0 +1,38 @@
+p<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+            $table->String('codigo', 10);
+            $table->integer('cantidad');
+            $table->decimal('precio', 8,2);
+            $table->timestamps();
+
+            
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+};
